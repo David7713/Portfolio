90233f02390e13c6729bdd58afb90d7cb23d6a72
David <davo00771339@gmail.com>
2024-09-07 16:45:00 +0400

    New Projects added

--- src/pages/Portfolio/data.js
+++ src/pages/Portfolio/data.js
@@ -5,90 +5,137 @@ import weatherImage from "../../assets/Portfolio-Screenshots/Weather-Screenshot.
 import RateShiftImage from "../../assets/Portfolio-Screenshots/RateShift-Screenshot.png"
 import SlotGameImage from "../../assets/Portfolio-Screenshots/SlotGame-Screenshot.png"
 import BookingApp from "../../assets/Portfolio-Screenshots/BookingApp-Screenshot.png"
+import PMDImage from "../../assets/Portfolio-Screenshots/PMDScreenshot.png"
+import DashboardIamge from "../../assets/Portfolio-Screenshots/DashboardScreenshot.jpg"
+import ConstructionImage from "../../assets/Portfolio-Screenshots/ConstructionScreenshot.jpg"
+import FinancialImage from "../../assets/Portfolio-Screenshots/FinancialScreenshot.jpg"
 
 
 const data = [
     {
         "id": 1,
-        "image":portalImage,
-        "title":"Portal",
-        "description":"Portal is a user-friendly tourism website",
-        "link":"https://david7713.github.io/Project-Portal/",
-        "github":"https://github.com/David7713/Project-Portal",
-        
-        
-    },
-    ,
-    {
+        "image": portalImage,
+        "title": "Portal",
+        "description": "Portal is a user-friendly tourism website",
+        "link": "https://david7713.github.io/Project-Portal/",
+        "github": "https://github.com/David7713/Project-Portal",
+
+
+    }, {
         "id": 2,
-        "image":beloteImage,
-        "title":"Belote",
-        "description":"An Information Website",
-        "link":"https://david7713.github.io/Belote-Club/",
-        "github":"https://github.com/David7713/Belote-Club"
-        
-        
+        "image": PMDImage,
+        "title": "PMD Solutions",
+        "description": "Built a website for PMD Solutions.",
+        "link": "https://pmdsolutions.org/",
+        "github": "https://pmdsolutions.org/"
+
+
     },
-   
- 
+
+
     {
         "id": 3,
-        "image":rareImage,
-        "title":"Rare",
-        "description":"E-Commerce Website",
-        "link":"https://david7713.github.io/Rare/",
-        "github":"https://github.com/David7713/Rare"
-        
-        
+        "image": rareImage,
+        "title": "Rare",
+        "description": "E-Commerce Website",
+        "link": "https://david7713.github.io/Rare/",
+        "github": "https://github.com/David7713/Rare"
+
+
     },
+
+
+    ,
     {
         "id": 4,
-        "image":weatherImage,
-        "title":"Weather",
-        "description":"Weather App with Forecast",
-        "link":"https://david7713.github.io/Weather-App/",
-        "github":"https://github.com/David7713/Weather-App"
-        
-        
-    }
+        "image": FinancialImage,
+        "title": "Financial App",
+        "description": "Comprehensive CRM for managing financial activities and reporting",
+        "link": "https://github.com/David7713/Financial-App",
+        "github": "https://github.com/David7713/Financial-App"
+
+
+    },
+
     ,
     {
         "id": 5,
-        "image":RateShiftImage,
-        "title":"RateShift",
-        "description":"Fast and efficient currency exchange platform.",
-        "link":"https://david7713.github.io/Rateshift/",
-        "github":"https://github.com/David7713/Rateshift"
-        
-        
+        "image": RateShiftImage,
+        "title": "RateShift",
+        "description": "Fast and efficient currency exchange platform.",
+        "link": "https://david7713.github.io/Rateshift/",
+        "github": "https://github.com/David7713/Rateshift"
+
+
     }
     ,
     {
         "id": 6,
-        "image":SlotGameImage,
-        "title":"Game",
-        "description":"An engaging slot game packed with adventures",
-        "link":"https://david7713.github.io/Game/",
-        "github":"https://github.com/David7713/Game"
-        
-        
+        "image": SlotGameImage,
+        "title": "Game",
+        "description": "An engaging slot game packed with adventures",
+        "link": "https://david7713.github.io/Game/",
+        "github": "https://github.com/David7713/Game"
+
+
     }
     ,
     {
         "id": 7,
-        "image":BookingApp,
-        "title":"Booking App",
-        "description":"Explore the world with our seamless travel app.",
-        "link":"https://david7713.github.io/Project/",
-        "github":"https://github.com/David7713/Project"
-        
-        
-    }
-   
- 
-    
-  
- 
- ]
+        "image": BookingApp,
+        "title": "Booking App",
+        "description": "Explore the world with our seamless travel app.",
+        "link": "https://david7713.github.io/Project/",
+        "github": "https://github.com/David7713/Project"
 
- export default data;
+
+    }
+    ,
+    {
+        "id": 8,
+        "image": beloteImage,
+        "title": "Belote",
+        "description": "An Information Website",
+        "link": "https://david7713.github.io/Belote-Club/",
+        "github": "https://github.com/David7713/Belote-Club"
+
+
+    },
+
+    {
+        "id": 9,
+        "image": DashboardIamge,
+        "title": "Admin Dashboard",
+        "description": "Robust admin dashboard with Node.js, Express.js, and MySQL for efficient management.",
+        "link": "https://github.com/David7713/Admin-Panel",
+        "github": "https://github.com/David7713/Admin-Panel"
+
+
+    }
+    ,
+    {
+        "id": 10,
+        "image": ConstructionImage,
+        "title": "Construction Website",
+        "description": "Professional website showcasing expertise in construction services.",
+        "link": "https://github.com/David7713/Construction-website",
+        "github": "https://github.com/David7713/Construction-website"
+
+
+    },
+    {
+        "id": 11,
+        "image": weatherImage,
+        "title": "Weather",
+        "description": "Weather App with Forecast",
+        "link": "https://david7713.github.io/Weather-App/",
+        "github": "https://github.com/David7713/Weather-App"
+
+
+    }
+
+
+
+]
+
+export default data;
